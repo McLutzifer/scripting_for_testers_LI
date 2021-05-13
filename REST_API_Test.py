352db4743744cb54ff48cc74bfcde195ea75289f
McLutzifer <lukasschumi@gmx.at>
2021-05-13 17:17:01 +0200

    add comments

--- REST_API_Test.py
+++ REST_API_Test.py
@@ -1,9 +1,14 @@
 import requests
 
 url = 'http://jsonplaceholder.typicode.com/photos'
+
+#### GET
 response = requests.get(url)
 #print(response.json())
 
+### POST
 jsonPayload = {'albumId':1, 'title':'test', 'url':'nothing.com', 'thumbnailUrl':'nothing.com'}
 new_response = requests.post(url, json=jsonPayload)
-print(response.json())
+print(new_response.json())
+
+#### PUT
