e8f7f1c1d18844b4669b3dc6ad3e1e19f026cc59
McLutzifer <lukasschumi@gmx.at>
2021-05-13 17:19:49 +0200

    delete

--- REST_API_Test.py
+++ REST_API_Test.py
@@ -8,7 +8,14 @@ response = requests.get(url)
 
 ### POST
 jsonPayload = {'albumId':1, 'title':'test', 'url':'nothing.com', 'thumbnailUrl':'nothing.com'}
-new_response = requests.post(url, json=jsonPayload)
-print(new_response.json())
+post_response = requests.post(url, json=jsonPayload)
+#print(post_response.json())
 
 #### PUT
+new_url = 'http://jsonplaceholder.typicode.com/photos/100'
+put_response = requests.put(new_url, json=jsonPayload)
+#print(put_response.json())
+
+### DELETE
+delete_response = requests.delete(new_url)
+print(delete_response.json())
